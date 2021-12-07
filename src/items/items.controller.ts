f0f13bf2511bc5a04159ea4b207333b99158fb9b
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2021-12-07 12:37:54 +0100

    Find all items

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -8,12 +8,16 @@ import {
   Put,
 } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
+import { ItemsService } from './items.service';
+import { ItemInterface } from './interfaces/item.interface';
 
 @Controller('items')
 export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+
   @Get()
-  find(): string {
-    return 'Get all items';
+  find(): ItemInterface[] {
+    return this.itemsService.find();
   }
 
   @Post()
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ItemInterface } from './interfaces/item.interface';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  private readonly items: ItemInterface[] = [];
+
+  find(): ItemInterface[] {
+    return this.items;
+  }
+}
