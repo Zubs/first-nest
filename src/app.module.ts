d7f8bcac132ce9bbfcaf3bd36cbe224227be6aaf
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2021-12-08 12:26:55 +0100

    Init mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsController } from './items/items.controller';
 import { ItemsService } from './items/items.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ItemsModule } from './items/items.module';
+import config from './config/keys';
 
 @Module({
-  imports: [],
+  imports: [ItemsModule, MongooseModule.forRoot(config.mongoURL)],
   controllers: [AppController, ItemsController],
   providers: [AppService, ItemsService],
 })
---
+++ src/config/keys.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoURL:
+    'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.mqtop.mongodb.net/first-nest?retryWrites=true&w=majority',
+};
